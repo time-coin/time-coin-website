5dbf892b7a3797d9febd733685b8abb064377c39
William Corless <wmcorless@gmail.com>
2025-10-22 15:06:25 -0700

    feat(api): serve peers at /api/peers from peers.json (for node discovery)

--- api/peers.js
+++ api/peers.js
@@ -5,7 +5,6 @@ module.exports = (req, res) => {
   try {
     const file = path.join(process.cwd(), 'peers.json');
     const body = fs.readFileSync(file, 'utf8');
-
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.setHeader('Cache-Control', 'public, max-age=60, stale-while-revalidate=300');
     res.status(200).send(body);
