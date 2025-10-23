cf8854536f8c81a5cd4ae0202ebb47121466c034
William Corless <wmcorless@gmail.com>
2025-10-23 11:06:18 -0700

    Fix peers format to string array

--- api/peers.js
+++ api/peers.js
@@ -1,14 +1,6 @@
-const fs = require('fs');
-const path = require('path');
+const peers = require('../peers.json');
 
 module.exports = (req, res) => {
-  try {
-    const file = path.join(process.cwd(), 'peers.json');
-    const body = fs.readFileSync(file, 'utf8');
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.setHeader('Cache-Control', 'public, max-age=60, stale-while-revalidate=300');
-    res.status(200).send(body);
-  } catch (e) {
-    res.status(500).json({ error: 'Failed to load peers.json', detail: String(e) });
-  }
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(peers);
 };
